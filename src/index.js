052e3516cd34d327c80340ad639c8725234423b1
Panther <marudhupandiyang@gmail.com>
2017-12-01 21:38:01 +0530

    add webpack, bable,eslint

--- src/index.js
+++ src/index.js
@@ -1,53 +1,45 @@
-'use strict';
+const optionsRegex = /(--[a-zA-Z\-]+ '.*?')|(--[a-zA-Z\-]+)|(-[a-zA-Z\-]+? '.+?')|('?[a-z]+:\/\/.*?'+?)|("?[a-z]+:\/\/.*?"+?)/g; // eslint-disable-line
+const urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/; // eslint-disable-line
 
-var options = require('./options.js').default;
-
-
-const optionsRegex = /(--[a-zA-Z\-]+ '.*?')|(--[a-zA-Z\-]+)|(-[a-zA-Z\-]+? '.+?')|('?[a-z]+:\/\/.*?'+?)|("?[a-z]+:\/\/.*?"+?)/g
-
-const urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/
 export function parse(command = '') {
   const trimmedCmd = command.trim();
 
   const parsedOptions = {
     url: '',
-    headers: {}
+    headers: {},
   };
 
   // quit if the command does not starts with curl
-  if(trimmedCmd.indexOf('curl') !== 0) {
-    console.error('Not a valid curl command');
+  if (trimmedCmd.indexOf('curl') !== 0) {
+    console.error('Not a valid curl command'); // eslint-disable-line
     return parsedOptions;
   }
 
-  var matches = trimmedCmd.match(optionsRegex);
-  console.log(JSON.stringify(matches));
+  const matches = trimmedCmd.match(optionsRegex);
 
-
-  matches.forEach((val = "") => {
-    val = val.trim();
+  matches.forEach((val = '') => {
+    let trimmedVal = val.trim();
 
     // URL MATCHER starts
-    if (['\'','"'].includes(val[0])) {
-      val = val.substr(1, val.length - 2);
+    if (['\'', '"'].includes(trimmedVal[0])) {
+      trimmedVal = trimmedVal.substr(1, trimmedVal.length - 2);
     }
 
-    var urlMatch = val.match(urlRegex);
+    const urlMatch = trimmedVal.match(urlRegex);
     if (urlMatch && urlMatch.length > 0) {
-      parsedOptions.url = urlMatch[0];
+      parsedOptions.url = urlMatch[0]; // eslint-disable-line
 
       const paramPosition = parsedOptions.url.indexOf('?');
-      if ( paramPosition !== -1) {
-        let splitUrl = parsedOptions.url.substr(0, paramPosition);
-        let paramsStr = parsedOptions.url.substr(paramPosition+1);
-        let params = paramsStr.split('&') || [];
-        console.log(params);
+      if (paramPosition !== -1) {
+        // const splitUrl = parsedOptions.url.substr(0, paramPosition);
+        const paramsStr = parsedOptions.url.substr(paramPosition + 1);
+        const params = paramsStr.split('&') || [];
 
         parsedOptions.queryString = {};
 
         params.forEach((param) => {
-          const splitParam = param.split("=");
-          parsedOptions.queryString[splitParam[0]] = splitParam[1];
+          const splitParam = param.split('='); // eslint-disable-line
+          parsedOptions.queryString[splitParam[0]] = splitParam[1]; // eslint-disable-line
         });
       }
       return;
@@ -55,16 +47,16 @@ export function parse(command = '') {
     // URL MATCHER ends
 
     // Other options starts
-    if (val.startsWith('-H ') || val.startsWith('--headers ')) {
-      let newVal = val.substr(val.indexOf(' ')).trim();
-      if (['\'','"'].includes(newVal[0])) {
+    if (trimmedVal.startsWith('-H ') || trimmedVal.startsWith('--headers ')) {
+      let newVal = trimmedVal.substr(trimmedVal.indexOf(' ')).trim();
+      if (['\'', '"'].includes(newVal[0])) {
         newVal = newVal.substr(1, newVal.length - 2);
       }
       const splitHeader = newVal.split(':');
       parsedOptions.headers[splitHeader[0].trim()] = splitHeader[1].trim();
-    } else if (val.startsWith('--data ') || val.startsWith('--data-ascii ') || val.startsWith('-d ') || val.startsWith('--data-raw ') || val.startsWith('--dta-urlencode ') || val.startsWith('--data-binary ')) {
-      let newVal = val.substr(val.indexOf(' ')).trim();
-      if (['\'','"'].includes(newVal[0])) {
+    } else if (trimmedVal.startsWith('--data ') || trimmedVal.startsWith('--data-ascii ') || trimmedVal.startsWith('-d ') || trimmedVal.startsWith('--data-raw ') || trimmedVal.startsWith('--dta-urlencode ') || trimmedVal.startsWith('--data-binary ')) {
+      let newVal = trimmedVal.substr(trimmedVal.indexOf(' ')).trim();
+      if (['\'', '"'].includes(newVal[0])) {
         newVal = newVal.substr(1, newVal.length - 2);
       }
       parsedOptions.body = newVal;
@@ -74,12 +66,12 @@ export function parse(command = '') {
 
   if (parsedOptions.body && parsedOptions.headers['content-type'] && parsedOptions.headers['content-type'].indexOf('application/json') !== -1) {
     try {
-      parsedOptions.body.replace('\\"','"');
-      parsedOptions.body.replace("\\'","'");
+      parsedOptions.body.replace('\\"', '"');
+      parsedOptions.body.replace("\\'", "'");
       parsedOptions.body = JSON.parse(parsedOptions.body);
-    } catch(ex){
+    } catch (ex) {
       // ignore json conversion error..
-      console.log('Cannot parse JSON Data ' + ex.message);
+      console.log('Cannot parse JSON Data ' + ex.message); // eslint-disable-line
     }
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var webpack = require('webpack')
+var Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -15,5 +17,20 @@ module.exports = {
       amd: 'lodash',
       root: '_'
     }
-  }
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {}
+      },
+    ],
+  },
+  "plugins": [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new Visualizer(),
+  ],
 };
